2a56b28c24cd0c74a7353c27eddc9cd439f7aa0f
ApostleTimz <bulusakwashikitimothy@gmail.com>
2025-07-16 16:31:59 +0100

    git@github.com:ApostleTimz/Allowance.git

---
+++ class.py
@@ -0,0 +1,48 @@
+''' 
+arithemetic operators include:
+Addition
+subtraction
+division
+multiplication
+floor exponentials
+modulus
+
+
+first_number = 5
+
+first_number += 4
+print(first_number)
+first_number -= 10
+print(first_number)
+frist_number *= 20
+print(first_number)
+first_number **= 30
+print(first_number)
+first_number %= 50
+print(first_number)
+first_number /= 60
+print(first_number)
+'''
+
+allowance = 2000
+
+allowance -= 400
+print(allowance)
+
+allowance += 100
+print(allowance)
+
+allowance -= 250
+print(allowance)
+
+allowance -= 1450 * 0.25
+print(allowance)
+
+allowance -= (1/3) * allowance 
+print(allowance)
+
+allowance //= 2
+allowance %= 100
+
+print(allowance)
+ 
